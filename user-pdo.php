783c7bf086b51e1457dc69e269c0c7fba33e218e
[enzo.mandine] <[enzo.mandine@laplateforme.io]>
2020-02-10 15:29:49 +0100

    Final push

--- user-pdo.php
+++ user-pdo.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-class user
+class userpdo
 {
     private $id;
     public $login = null;
@@ -8,18 +8,39 @@ class user
     public $firstname = null;
     public $lastname = null;
 
+    // private $dsn = null;
+    // private $user = null;
+    // private $password = null;
+
+    // private function connectdb()
+    // {
+    //     try {
+    //         $dbh = new PDO($this->dsn, $this->user, $this->password);
+    //         echo 'Connecté';
+    //     } catch (PDOException $e) {
+    //         echo 'Connexion échouée : ' . $e->getMessage();
+    //     }
+    // }
+
+    // public function initDB($dsn, $user, $password)
+    // {
+    //     $this->dsn = $dsn;
+    //     $this->password = $password;
+    //     $this->user = $user;
+    //     $this->connectdb();
+    // }
+
     public function register($login, $email, $firstname, $lastname, $password)
     {
         if (!empty($login) && !empty($email) && !empty($firstname) && !empty($lastname) && !empty($password)) {
-            $connect = mysqli_connect("localhost", "root", "", "poo");
-            $request = "SELECT login FROM user WHERE login = '" . $login . "' ";
-            $query = mysqli_query($connect, $request);
-            $result = mysqli_fetch_all($query);
-            if (empty($result)) {
-                $request = "INSERT INTO `user`(`id`, `login`, `email`, `firstname`, `lastname`, `password`) VALUES (NULL, '" . $login . "', '" . $email . "', '" . $firstname . "', '" . $lastname . "', '" . password_hash($password, PASSWORD_DEFAULT) . "')";
-                $query = mysqli_query($connect, $request);
+            $conn = new PDO("mysql:host=localhost;dbname=poo", "root", "");
+            $sql = "SELECT * FROM user WHERE login = '" . $login . "' ";
+            $usr_data = $conn->query($sql)->fetch(PDO::FETCH_ASSOC);
+            if ($usr_data == false) {
+                $sql = "INSERT INTO `user`(`id`, `login`, `email`, `firstname`, `lastname`, `password`) VALUES (NULL, '" . $login . "', '" . $email . "', '" . $firstname . "', '" . $lastname . "', '" . password_hash($password, PASSWORD_DEFAULT) . "')";
+                $usr_insert = $conn->query($sql);
                 echo "Inscription Validée";
-                return [$this->id, $this->login, $this->email, $this->firstname, $this->lastname, $this->password];
+                return [$login, $email, $firstname, $lastname];
             } else {
                 echo "Login déja pris!";
             }
@@ -31,20 +52,17 @@ class user
     public function connect($login, $password)
     {
         if (!empty($login) && !empty($password)) {
-            $connect = mysqli_connect("localhost", "root", "", "poo");
-            $request = "SELECT * FROM user WHERE login = '" . $login . "' ";
-            var_dump($request);
-            $query = mysqli_query($connect, $request);
-            $result = mysqli_fetch_all($query);
-            var_dump($result);
-            if (!empty($result)) {
-                if (password_verify($password, $result[0][5])) {
-                    $this->id = $result[0][0];
-                    $this->login = $result[0][1];
-                    $this->email = $result[0][2];
-                    $this->firstname = $result[0][3];
-                    $this->lastname = $result[0][4];
-                    $this->password = $result[0][5];
+            $conn = new PDO("mysql:host=localhost;dbname=poo", "root", "");
+            $sql = "SELECT * FROM user WHERE login = '" . $login . "' ";
+            $usr_data = $conn->query($sql)->fetch(PDO::FETCH_ASSOC);
+            if ($conn->query($sql) == true) {
+                if (password_verify($password, $usr_data["password"])) {
+                    $this->id = $usr_data["id"];
+                    $this->login = $usr_data["login"];
+                    $this->email = $usr_data["email"];
+                    $this->firstname = $usr_data["firstname"];
+                    $this->lastname = $usr_data["lastname"];
+                    $this->password = $usr_data["password"];
                     echo "Connexion validée<br>";
                     return [$this->id, $this->login, $this->email, $this->firstname, $this->lastname, $this->password];
                 } else {
@@ -66,17 +84,17 @@ class user
             $this->email = NULL;
             $this->firstname = NULL;
             $this->lastname = NULL;
+            echo "Déconnexion";
             return "Déconnexion";
         }
     }
 
     public function delete()
     {
-        $connect = mysqli_connect("localhost", "root", "", "poo");
-        $request = "DELETE FROM user WHERE id = " . $this->id;
-        $query = mysqli_query($connect, $request);
-
-        if ($query == true) {
+        $conn = new PDO("mysql:host=localhost;dbname=poo", "root", "");
+        $sql = "DELETE FROM user WHERE id = " . $this->id;
+        $usr_data = $conn->query($sql);
+        if ($usr_data == true) {
             $this->disconnect();
         }
     }
@@ -84,54 +102,42 @@ class user
     public function update($login, $email, $firstname, $lastname, $password)
     {
         if ($this->id != NULL) {
-            $connect = mysqli_connect("localhost", "root", "", "poo");
-            $request = "SELECT * FROM user WHERE id = " . $this->id;
-            $query = mysqli_query($connect, $request);
-            $result = mysqli_fetch_all($query);
-
-            if (!empty($result)) {
+            $conn = new PDO("mysql:host=localhost;dbname=poo", "root", "");
+            $sql = "SELECT * FROM user WHERE id =" . $this->id;
+            $usr_data = $conn->query($sql)->fetch(PDO::FETCH_ASSOC);
+            if (!empty($usr_data)) {
                 if ($this->login != $login) {
-
-
                     $this->login = $login;
                     echo "login modifé<br>";
                 } else {
                     echo "login identique<br>";
                 }
                 if ($this->email != $email) {
-
-
                     $this->email = $email;
                     echo "email modifé<br>";
                 } else {
                     echo "email identique<br>";
                 }
                 if ($this->firstname != $firstname) {
-
-
                     $this->firstname = $firstname;
                     echo "firstname modifé<br>";
                 } else {
                     echo "firstname identique<br>";
                 }
                 if ($this->lastname != $lastname) {
-
-
                     $this->lastname = $lastname;
                     echo "lastname modifé<br>";
                 } else {
                     echo "lastname identique<br>";
                 }
                 if ($this->password != password_verify($password, $this->password)) {
-
-
                     $this->password = password_hash($password, PASSWORD_DEFAULT);
                     echo "password modifé<br>";
                 } else {
                     echo "password identique<br>";
                 }
-                $request = "UPDATE `user` SET `login`= '" . $this->login . "', `email`= '" . $this->email . "', `firstname`= '" . $this->firstname . "', `lastname`= '" . $this->lastname . "', `password`= '" . password_hash($this->password, PASSWORD_DEFAULT) . "' WHERE id = " . $this->id;
-                $query = mysqli_query($connect, $request);
+                $sql = "UPDATE `user` SET `login`= '" . $this->login . "', `email`= '" . $this->email . "', `firstname`= '" . $this->firstname . "', `lastname`= '" . $this->lastname . "', `password`= '" . password_hash($this->password, PASSWORD_DEFAULT) . "' WHERE id = " . $this->id;
+                $usr_data = $conn->query($sql);
             }
         }
     }
@@ -169,17 +175,16 @@ class user
         return $this->lastname;
     }
 
-    public function refresh()
+    public function refresh($login)
     {
-        $connect = mysqli_connect("localhost", "root", "", "poo");
-        $request = "SELECT * FROM user WHERE id = " . $this->id;
-        $query = mysqli_query($connect, $request);
-        $result = mysqli_fetch_all($query);
-        $this->id = $result[0][0];
-        $this->login = $result[0][1];
-        $this->email = $result[0][2];
-        $this->firstname = $result[0][3];
-        $this->lastname = $result[0][4];
-        $this->password = $result[0][5];
+        $conn = new PDO("mysql:host=localhost;dbname=poo", "root", "");
+        $sql = "SELECT * FROM user WHERE login = '" . $login . "' ";
+        $usr_data = $conn->query($sql)->fetch(PDO::FETCH_ASSOC);
+        $this->id = $usr_data["id"];
+        $this->login = $usr_data["login"];
+        $this->email = $usr_data["email"];
+        $this->firstname = $usr_data["firstname"];
+        $this->lastname = $usr_data["lastname"];
+        $this->password = $usr_data["password"];
     }
 }
